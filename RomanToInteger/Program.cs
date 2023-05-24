70e183aebc1c502ddec6cba2a89ba82bdf2dcd05
GeorgiKatsarov <100018528+GeorgiKatsarov@users.noreply.github.com>
2023-05-25 01:37:23 +0300

    FirstLeetProblem

---
+++ RomanToInteger/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        int returnInt = 0;
+        List<int> listNumbers = new List<int>();
+        foreach (char c in s)
+        {
+            switch (c)
+            {
+                case 'I':
+                    listNumbers.Add(1);
+                    break;
+                case 'V':
+                    listNumbers.Add(5);
+                    break;
+                case 'X':
+                    listNumbers.Add(10);
+                    break;
+                case 'L':
+                    listNumbers.Add(50);
+                    break;
+                case 'C':
+                    listNumbers.Add(100);
+                    break;
+                case 'D':
+                    listNumbers.Add(500);
+                    break;
+                case 'M':
+                    listNumbers.Add(1000);
+                    break;
+            }
+        }
+        for (int i = 0; i < listNumbers.Count; i++)
+        {
+            if (i == listNumbers.Count - 1 || listNumbers[i] >= listNumbers[i + 1])
+            {
+                returnInt += listNumbers[i];
+            }
+            else
+            {
+                returnInt -= listNumbers[i];
+            }
+        }
+
+        return returnInt;
+    }
+}
